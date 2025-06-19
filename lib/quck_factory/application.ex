372274d9d9f70721f316785b60bc06b26025d23a
Roman Heinrich <roman.heinrich@gmail.com>
2025-06-19 19:30:25 +0200

    Feat: more robust starting for QuickFactory.Counters module

--- lib/quck_factory/application.ex
+++ lib/quck_factory/application.ex
@@ -3,6 +3,8 @@ defmodule QuickFactory.Application do
 
   @impl true
   def start(_type, _args) do
+    QuickFactory.Counters.start()
+
     Supervisor.start_link([QuickFactory.Sequence],
       strategy: :one_for_one,
       name: __MODULE__.Supervisor
--- test/quick_factory/schema_counter_test.exs
+++ test/quick_factory/schema_counter_test.exs
@@ -1,11 +1,5 @@
 defmodule QuickFactory.CountersTest do
   use ExUnit.Case
 
-  setup_all do
-    :ok = QuickFactory.Counters.start()
-
-    %{}
-  end
-
   doctest QuickFactory.Counters
 end

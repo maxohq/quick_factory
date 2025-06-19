de3d3a0c54a854b76f162be98e0a02561902c801
Roman Heinrich <roman.heinrich@gmail.com>
2025-06-19 17:56:54 +0200

    Chore: prepare release on hex.pm

--- mix.exs
+++ mix.exs
@@ -1,14 +1,20 @@
 defmodule QuickFactory.MixProject do
   use Mix.Project
+  @github_url "https://github.com/maxohq/quick_factory"
+  @version "0.1.0"
 
   def project do
     [
       app: :quick_factory,
-      version: "0.1.0",
+      version: @version,
+      description:
+        "QuickFactory - plain yet powerful factory for Ecto with support for changesets",
+      source_url: @github_url,
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -16,6 +22,17 @@ defmodule QuickFactory.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib mix.exs README* CHANGELOG*),
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @github_url,
+        "Changelog" => "#{@github_url}/blob/main/CHANGELOG.md"
+      }
     ]
   end
 
@@ -29,7 +46,8 @@ defmodule QuickFactory.MixProject do
       {:ecto, "~> 3.0"},
       {:faker, ">= 0.0.0"},
       {:ecto_sql, "~> 3.0", only: [:test, :dev], optional: true},
-      {:postgrex, "~> 0.16", only: [:test, :dev], optional: true}
+      {:postgrex, "~> 0.16", only: [:test, :dev], optional: true},
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
     ]
   end
 end
